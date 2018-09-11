ed9755956ca84c56caf3a242df6d7f85f586e313
Hitesh Kumar Dasika <hdasika@mathworks.com>
2018-09-11 10:59:34 -0400

    Connecting to fb using spring social. For now, only with fixed user

---
+++ connectToFb/src/main/java/hello/HelloController.java
@@ -0,0 +1,50 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        String [] fields = { "id", "email",  "first_name", "last_name" };
+        model.addAttribute("facebookProfile", facebook.fetchObject("me", User.class,fields));
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "hello";
+    }
+
+    @SpringBootApplication
+    public static class FbLogin {
+        public static void main(String[] args) {
+            SpringApplication.run(FbLogin.class, args);
+        }
+
+    }
+}
+
+
